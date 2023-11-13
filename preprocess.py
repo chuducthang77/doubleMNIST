8af76a421382accf400b63068d579b96404562f4
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-13 14:51:35 +0700

    Finish the visualization code and update the preprocess code

--- preprocess.py
+++ preprocess.py
@@ -2,46 +2,59 @@ import numpy as np
 import os
 from PIL import Image
 
+# Load the train set
 train_X = np.load("./train_X.npy")
 train_Y = np.load("./train_Y.npy")
 train_bboxes = np.load("./train_bboxes.npy")
 modified_train_bboxes = np.copy(train_bboxes)
-modified_train_bboxes[:, :, 0] = train_bboxes[:, :, 1] + (train_bboxes[:, :, 3] - train_bboxes[:, :, 1])/2
-modified_train_bboxes[:, :, 1] = train_bboxes[:, :, 0] + (train_bboxes[:, :, 2] - train_bboxes[:, :, 0])/2
+
+# Change the format from (y_min, x_min, y_max, x_max) to (x_center, y_center, width, height)
+modified_train_bboxes[:, :, 0] = (train_bboxes[:, :, 1] + (train_bboxes[:, :, 3] - train_bboxes[:, :, 1])/2)
+modified_train_bboxes[:, :, 1] = (train_bboxes[:, :, 0] + (train_bboxes[:, :, 2] - train_bboxes[:, :, 0])/2)
 modified_train_bboxes[:, :, 2] = (train_bboxes[:, :, 3] - train_bboxes[:, :, 1])
 modified_train_bboxes[:, :, 3] = (train_bboxes[:, :, 2] - train_bboxes[:, :, 0])
 
+# Change the scale to 0-1
+modified_train_bboxes = modified_train_bboxes / 64
+
+# Concatenate the label
 train_Y = train_Y.reshape(train_Y.shape[0], train_Y.shape[1], 1)
 train_Y = np.concatenate((train_Y, modified_train_bboxes), axis=2)
-for i in range(len(train_X)):
-    im = Image.fromarray(train_X[i])
-    im.save(f"../datasets/custom/images/train/{i}.jpeg")
-    with open(f'../datasets/custom/labels/train/{i}.txt', 'w') as f:
-        f.write(str(train_Y[i]))
 
+# Save the img as well as the label
+for i in range(len(train_Y)):
+    im = Image.fromarray(train_X[i].reshape(64, 64, 3))
+    im.save(f"../datasets/custom/images/train/{i}.jpeg")
+    np.savetxt(f'../datasets/custom/labels/train/{i}.txt', train_Y[i])
+
+# Save the numpy file
 np.save('m_train_Y.npy',train_Y)
 
 
-
+# Load the validation file
 valid_X = np.load("./valid_X.npy")
 valid_Y = np.load("./valid_Y.npy")
 valid_bboxes = np.load("./valid_bboxes.npy")
 modified_valid_bboxes = np.copy(valid_bboxes)
-modified_valid_bboxes[:, :, 0] = valid_bboxes[:, :, 1] + (valid_bboxes[:, :, 3] - valid_bboxes[:, :, 1])/2
-modified_valid_bboxes[:, :, 1] = valid_bboxes[:, :, 0] + (valid_bboxes[:, :, 2] - valid_bboxes[:, :, 0])/2
+
+# Change the format from (y_min, x_min, y_max, x_max) to (x_center, y_center, width, height)
+modified_valid_bboxes[:, :, 0] = (valid_bboxes[:, :, 1] + (valid_bboxes[:, :, 3] - valid_bboxes[:, :, 1])/2)
+modified_valid_bboxes[:, :, 1] = (valid_bboxes[:, :, 0] + (valid_bboxes[:, :, 2] - valid_bboxes[:, :, 0])/2)
 modified_valid_bboxes[:, :, 2] = (valid_bboxes[:, :, 3] - valid_bboxes[:, :, 1])
 modified_valid_bboxes[:, :, 3] = (valid_bboxes[:, :, 2] - valid_bboxes[:, :, 0])
 
+# Change the scale to 0-1
+modified_valid_bboxes = modified_valid_bboxes / 64
+
+# Concatenate the label
 valid_Y = valid_Y.reshape(valid_Y.shape[0], valid_Y.shape[1], 1)
 valid_Y = np.concatenate((valid_Y, modified_valid_bboxes), axis=2)
 
-valid_Y = valid_Y.reshape(valid_Y.shape[0], valid_Y.shape[1], 1)
-valid_Y = np.concatenate((valid_Y, modified_valid_bboxes), axis=2)
-for i in range(len(valid_X)):
-    im = Image.fromarray(valid_X[i])
+# Save the img as well as the label
+for i in range(len(valid_Y)):
+    im = Image.fromarray(valid_X[i].reshape(64, 64, 3))
     im.save(f"../datasets/custom/images/valid/{i}.jpeg")
-    with open(f'../datasets/custom/labels/valid/{i}.txt', 'w') as f:
-        f.write(str(valid_Y[i]))
+    np.savetxt(f'../datasets/custom/labels/valid/{i}.txt', valid_Y[i])
 
-
+# Save the numpy file
 np.save('m_valid_Y.npy',valid_Y)
---
+++ visualization.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+from PIL import Image
+
+
+# Load the image
+images_dir = "../zidane.jpg"
+labels_dir = "../zidane.txt"
+
+img = np.asarray(Image.open(images_dir))
+labels = np.loadtxt(labels_dir)
+
+height, width, _ = img.shape
+
+# Rescale
+labels[:, 1] *= width # x_center
+labels[:, 3] *= width # width
+labels[:, 2] *= height # y_center
+labels[:, 4] *= height # height
+
+# Change from the format (x_center, y_center, width, height) to (y_min, x_min, y_max, x_max)
+y_min = labels[:, 2] - labels[:, 4] / 2
+y_max = labels[:, 2] + labels[:, 4] / 2
+x_min = labels[:, 1] - labels[:, 3] / 2
+x_max = labels[:, 1] + labels[:, 3] / 2
+
+# Plot
+fig, ax = plt.subplots()
+ax.imshow(img)
+for i in range(len(x_min)):
+    rect = patches.Rectangle((x_min[i], y_min[i]), labels[:, 3][i], labels[:, 4][i], edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+plt.show()
